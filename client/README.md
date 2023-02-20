6c0c4775411fb3cf44bb9eb6a78805d59a150694
Your Name <you@example.com>
2023-02-20 11:03:43 -0300

    feat: complete config feature toggle

--- client/README.md
+++ client/README.md
@@ -1,1 +1,7 @@
 - Install firebase sdk
+
+
+OBS:
+ - Firebase Remote Config , um produto que ajuda você a modificar seu aplicativo sem implantar uma nova versão(https://firebase.blog/posts/2018/11/using-cloud-functions-to-update-remote)
+
+ https://codeburst.io/firebase-remote-config-for-web-d3d0f6ed7b4
--- client/src/services/firebase.ts
+++ client/src/services/firebase.ts
@@ -14,8 +14,12 @@ export const app: FirebaseApp = initializeApp(firebaseConfig);
 export const remoteConfig: RemoteConfig = getRemoteConfig(app);
 remoteConfig.settings = {
   fetchTimeoutMillis: 60000,
-  minimumFetchIntervalMillis: 3600000
+  minimumFetchIntervalMillis: 1
 }
+
+remoteConfig.defaultConfig = ({
+ 'app_super_awesome_feature': false
+})
 
 export const fetchRemoteConfig = async (): Promise<void> => {
   await fetchAndActivate(remoteConfig);
