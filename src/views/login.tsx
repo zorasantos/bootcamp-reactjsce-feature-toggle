6e41e1af314bd992099f6776f4381e31c9d5ff53
Your Name <you@example.com>
2023-02-22 11:12:52 -0300

    feat: create button component

--- src/views/login.tsx
+++ src/views/login.tsx
@@ -1,25 +1,47 @@
 import { useFeature } from "../hooks/useFeature";
 import IconGoogle from "../assets/iconGoogle.svg"
 import IconLinkedin from "../assets/iconLinkedin.svg"
+import { Button } from "../components/Button";
 
 
 export const LoginPage = () => {
 
+  const socialButtonStyle = "w-full flex flex-nowrap items-center justify-center gap-2 text-gray-400 border dark:border-gray-600 hover:dark:bg-gray-700 hover:text-white rounded-lg px-5 py-2"
+
   const {  isEnable } = useFeature()
+
+  const handleButtonGoogle = () => {
+    console.log('Botão google clicado!');
+  };
+
+  const handleButtonLinkedin = () => {
+    console.log('Botão linkedin clicado!');
+  };
+
+  const handleSubmit = () => {
+    console.log('Botão submit clicado!');
+  };
 
   return (
     <div className="flex flex-col gap-5 w-[520px] h-[556px] bg-[#1F2937] rounded-lg p-6">
       <h1 className="text-white text-[1.2rem] font-bold">Welcome back</h1>
       <div className="flex flex-col md:flex-row gap-6">
-        {isEnable("new_API_login") && <button className="w-full flex flex-nowrap items-center justify-center gap-2 text-gray-400 border dark:border-gray-600 hover:dark:bg-gray-700 hover:text-white rounded-lg px-5 py-2">
-          <img src={IconGoogle} alt="Google Icon" />
-          Login with Google
-        </button>}
 
-        <button className="w-full flex flex-nowrap items-center justify-center gap-2 text-gray-400 border dark:border-gray-600 hover:dark:bg-gray-700 hover:text-white rounded-lg px-5 py-2">
-          <img src={IconLinkedin} alt="Linkedin Icon" />
-          Login with Linkedin
-        </button>
+        {isEnable("new_API_login") &&
+          <Button
+            text="Login with Google"
+            pathIcon={IconGoogle}
+            onClick={handleButtonGoogle}
+            className={socialButtonStyle}
+          />
+        }
+
+        <Button
+          text="Login with Linkedin"
+          pathIcon={IconLinkedin}
+          onClick={handleButtonLinkedin}
+          className={socialButtonStyle}
+        />
       </div>
       <div className="relative flex py-5 items-center">
         <div className="flex-grow border-2 dark:border-gray-600" />
@@ -37,7 +59,12 @@ export const LoginPage = () => {
           <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
           <input type="password" id="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
         </div>
-        <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account Or Login</button>
+        <Button
+          type="submit"
+          className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          text="Login"
+          onClick={handleSubmit}
+        />
       </form>
     </div>
   );
